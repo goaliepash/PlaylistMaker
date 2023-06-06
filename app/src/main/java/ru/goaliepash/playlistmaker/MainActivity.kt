00c5e3660dcdcc23d412a16a1235805d9c75dd64
goaliepash <goaltenders@yandex.ru>
2023-06-06 09:47:15 +0300

    feat: сделать кнопки на главном экране интерактивными

--- app/src/main/java/ru/goaliepash/playlistmaker/MainActivity.kt
+++ app/src/main/java/ru/goaliepash/playlistmaker/MainActivity.kt
@@ -1,11 +1,54 @@
 package ru.goaliepash.playlistmaker
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initUI()
+    }
+
+    private fun initUI() {
+        initSearchButton()
+        initMediaLibraryButton()
+        initSettingsButton()
+    }
+
+    private fun initSearchButton() {
+        val searchButton = findViewById<Button>(R.id.search_button)
+        val searchButtonClickListener = View.OnClickListener {
+            Toast
+                .makeText(
+                    this@MainActivity,
+                    getString(R.string.search_button_click),
+                    Toast.LENGTH_SHORT
+                )
+                .show()
+        }
+        searchButton.setOnClickListener(searchButtonClickListener)
+    }
+
+    private fun initMediaLibraryButton() {
+        val mediaLibraryButton = findViewById<Button>(R.id.media_library_button)
+        mediaLibraryButton.setOnClickListener {
+            Toast
+                .makeText(this, getString(R.string.media_library_button_click), Toast.LENGTH_SHORT)
+                .show()
+        }
+    }
+
+    private fun initSettingsButton() {
+        val settingsButton = findViewById<Button>(R.id.settings_button)
+        settingsButton.setOnClickListener {
+            Toast
+                .makeText(this, getString(R.string.settings_button_click), Toast.LENGTH_SHORT)
+                .show()
+        }
     }
 }
