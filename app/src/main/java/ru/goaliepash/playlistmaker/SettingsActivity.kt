90dea803db5fce30e862d319d57c2ed55d78ccbd
goaliepash <goaltenders@yandex.ru>
2023-06-19 10:32:40 +0300

    feat: добавить обработчики нажатия на кнопки в настройках

--- app/src/main/java/ru/goaliepash/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/ru/goaliepash/playlistmaker/SettingsActivity.kt
@@ -1,5 +1,7 @@
 package ru.goaliepash.playlistmaker
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
@@ -14,10 +16,55 @@ class SettingsActivity : AppCompatActivity() {
 
     private fun initUI() {
         initImageViewBack()
+        initImageViewShareApp()
+        initImageViewSupport()
+        initImageViewTermsOfUse()
     }
 
     private fun initImageViewBack() {
         val imageViewBack = findViewById<ImageView>(R.id.image_view_back)
         imageViewBack.setOnClickListener { finish() }
+    }
+
+    private fun initImageViewShareApp() {
+        val imageViewShareApp = findViewById<ImageView>(R.id.image_view_share_app)
+        imageViewShareApp.setOnClickListener { imageViewShareAppOnClick() }
+    }
+
+    private fun initImageViewSupport() {
+        val imageViewSupport = findViewById<ImageView>(R.id.image_view_support)
+        imageViewSupport.setOnClickListener { imageViewSupportOnClick() }
+    }
+
+    private fun initImageViewTermsOfUse() {
+        val imageViewTermsOfUse = findViewById<ImageView>(R.id.image_view_terms_of_use)
+        imageViewTermsOfUse.setOnClickListener { imageViewTermsOfUseOnClick() }
+    }
+
+    private fun imageViewShareAppOnClick() {
+        val intent = Intent().apply {
+            action = Intent.ACTION_SEND
+            putExtra(Intent.EXTRA_TEXT, getString(R.string.yp_android_course_url))
+            type = "text/plain"
+        }
+        startActivity(Intent.createChooser(intent, null))
+    }
+
+    private fun imageViewSupportOnClick() {
+        val intent = Intent().apply {
+            action = Intent.ACTION_SENDTO
+            data = Uri.parse("mailto:")
+            putExtra(Intent.EXTRA_EMAIL, arrayOf(getString(R.string.student_email)))
+            putExtra(Intent.EXTRA_TEXT, getString(R.string.message_to_developers))
+        }
+        startActivity(intent)
+    }
+
+    private fun imageViewTermsOfUseOnClick() {
+        val intent = Intent().apply {
+            action = Intent.ACTION_VIEW
+            data = Uri.parse(getString(R.string.yandex_offer_url))
+        }
+        startActivity(intent)
     }
 }
