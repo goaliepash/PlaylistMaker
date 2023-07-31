4ece8108fad843f6ac3c710e36676c683a4e508a
goaliepash <goaltenders@yandex.ru>
2023-07-31 09:10:17 +0300

    feat: история поиска по композициям

---
+++ app/src/main/java/ru/goaliepash/playlistmaker/SearchHistory.kt
@@ -0,0 +1,33 @@
+package ru.goaliepash.playlistmaker
+
+import android.content.SharedPreferences
+import com.google.gson.Gson
+import ru.goaliepash.playlistmaker.model.Track
+
+class SearchHistory(private val sharedPreferences: SharedPreferences) {
+
+    fun add(searchHistoryTracks: List<Track>) {
+        sharedPreferences.edit().putString(SEARCH_HISTORY_KEY, createJsonFromTrackList(searchHistoryTracks)).apply()
+    }
+
+    fun get(): Array<Track> {
+        return createTracksFromJson(sharedPreferences.getString(SEARCH_HISTORY_KEY, JSON_EMPTY_LIST) ?: JSON_EMPTY_LIST)
+    }
+
+    fun clear() {
+        sharedPreferences.edit().clear().apply()
+    }
+
+    private fun createJsonFromTrackList(tracks: List<Track>): String {
+        return Gson().toJson(tracks)
+    }
+
+    private fun createTracksFromJson(json: String): Array<Track> {
+        return Gson().fromJson(json, Array<Track>::class.java)
+    }
+
+    companion object {
+        private const val SEARCH_HISTORY_KEY = "SEARCH_HISTORY_KEY"
+        private const val JSON_EMPTY_LIST = "[]"
+    }
+}
